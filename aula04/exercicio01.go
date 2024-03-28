f9633a39c026b7542982ded0f8d63219fe6dff7e
Angela Almeida <angela.almeida@sap.com>
2024-03-28 14:52:57 -0300

    adicionando aula 04

---
+++ aula04/exercicio01.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+type Funcionario struct {
+	nome    string
+	idade   int
+	funcao  string
+	salario float64
+}
+
+func main() {
+	funcionario1 := Funcionario{"Larissa", 22, "Suport Engineer", 2000}
+	funcionario2 := Funcionario{"Melissa", 22, "Support Intern", 1000}
+	funcionario3 := Funcionario{"Rodrigo", 23, "Separador", 2100}
+
+	fmt.Println(funcionario1)
+	fmt.Println(funcionario2)
+	fmt.Println(funcionario3)
+}
